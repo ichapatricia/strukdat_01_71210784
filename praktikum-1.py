4e915ef033941db5bf8bb19482e7eb9c9cd11578
EEII7 <93314501+ichapatricia@users.noreply.github.com>
2022-08-29 19:13:27 +0700

    Add files via upload

---
+++ praktikum-1.py
@@ -0,0 +1,24 @@
+while True:
+
+    print('Berikut menu di kalkulator')
+    print('1.Tambah')
+    print('2. Kurang')
+    print('3. Kali')
+    print('4.Bagi')
+    print('Krtik Q untuk menghentikan')
+
+    n = int(input("Masukkan pilihan: "))
+
+    if n == 'Q':
+        break
+
+    a = int(input('a: '))
+    b = int(input('b:'))
+    if n == 1:
+        print(a+b)
+    elif n == 2:
+        print(a-b)
+    elif n == 3:
+        print(a*b)
+    elif n ==4:
+        print(a/b)
