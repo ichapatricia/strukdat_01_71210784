20e5233c57c619944a8cb7527378f9c9a0fb375c
EEII7 <93314501+ichapatricia@users.noreply.github.com>
2022-08-29 19:40:02 +0700

    upload ulang tugas

--- praktikum-1.py
+++ praktikum-1.py
@@ -7,18 +7,18 @@ while True:
     print('4.Bagi')
     print('Krtik Q untuk menghentikan')
 
-    n = int(input("Masukkan pilihan: "))
+    n = input("Masukkan pilihan: ")
 
     if n == 'Q':
         break
 
     a = int(input('a: '))
     b = int(input('b:'))
-    if n == 1:
+    if n == "1":
         print(a+b)
-    elif n == 2:
+    elif n == "2":
         print(a-b)
-    elif n == 3:
+    elif n == "3":
         print(a*b)
-    elif n ==4:
+    elif n == "4":
         print(a/b)
